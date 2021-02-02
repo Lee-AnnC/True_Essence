4b838289c79be64e1d359a8b78a339c93f2ad642
LEE-ANN CLARK <leeann74c@gmail.com>
2021-02-02 14:59:21 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IGPpNEqcNrK8sFkzLCRtlXeKS8Xp7pJ0PwXz3NVtZev3k9N2vQJK77sDNPawqJeJl6YT6p1qcfl3dh5MWGB5lbE00p24ZPndy',
+        
     }
 
     return render(request, template, context)
