9d9ac8b2a5ab429ab1b96e43a7cee22a30679314
LEE-ANN CLARK <leeann74c@gmail.com>
2021-02-16 08:31:08 +0000

    Added online shop, moved all categories and sub levels to online shop

--- shop/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
-def shop(request):
-    """ A view to return the online shop page """
-
-    return render(request, 'shop/shop.html')
--- true_essence/urls.py
+++ true_essence/urls.py
@@ -27,5 +27,4 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
-    path('shop/', include('shop.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
