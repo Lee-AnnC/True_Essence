593e58fbdbb3648a5d871244a922a5c2532c7d13
LEE-ANN CLARK <leeann74c@gmail.com>
2021-03-02 22:16:55 +0000

    deployment -part2

--- true_essence/settings.py
+++ true_essence/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '88n6d_3dv8vea^j76odh&)t$-rnqwci-)09#kf@htvna*)j$os'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['true-essence.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -123,14 +123,19 @@ WSGI_APPLICATION = 'true_essence.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
