468efc1d0b78d7d76e65608795421e928cfdb8f3
LEE-ANN CLARK <leeann74c@gmail.com>
2021-03-04 17:14:05 +0000

    base css and login/reg refactoring

--- true_essence/settings.py
+++ true_essence/settings.py
@@ -187,13 +187,12 @@ if 'USE_AWS' in os.environ:
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
     }
-    
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'true-esssence'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME, AWS_S3_REGION_NAME}.s3.amazonaws.com'
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
